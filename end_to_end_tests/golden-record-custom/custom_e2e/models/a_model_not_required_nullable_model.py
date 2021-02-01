817a79ef436bcb80a57ee3ee411b83ea8908e143
Dylan Anthony <contact@dylananthony.com>
2021-02-01 09:24:50 -0700

    chore: Update e2e record after merge with main

--- end_to_end_tests/golden-record-custom/custom_e2e/models/a_model_not_required_nullable_model.py
+++ end_to_end_tests/golden-record-custom/custom_e2e/models/a_model_not_required_nullable_model.py
@@ -1,6 +1,10 @@
-from typing import Any, Dict, List, Type, TypeVar
+from typing import Any, Dict, List, Type, TypeVar, Union
 
 import attr
+
+from ..models.an_enum import AnEnum
+from ..models.an_int_enum import AnIntEnum
+from ..types import UNSET, Unset
 
 T = TypeVar("T", bound="AModelNotRequiredNullableModel")
 
@@ -9,20 +13,59 @@ T = TypeVar("T", bound="AModelNotRequiredNullableModel")
 class AModelNotRequiredNullableModel:
     """  """
 
+    a_property: Union[Unset, AnEnum, AnIntEnum] = UNSET
     additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
+        a_property: Union[Unset, AnEnum, AnIntEnum]
+        if isinstance(self.a_property, Unset):
+            a_property = UNSET
+        elif isinstance(self.a_property, AnEnum):
+            a_property = UNSET
+            if not isinstance(self.a_property, Unset):
+                a_property = self.a_property
+
+        else:
+            a_property = UNSET
+            if not isinstance(self.a_property, Unset):
+                a_property = self.a_property
 
         field_dict: Dict[str, Any] = {}
         field_dict.update(self.additional_properties)
         field_dict.update({})
+        if a_property is not UNSET:
+            field_dict["a_property"] = a_property
 
         return field_dict
 
     @classmethod
     def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
         d = src_dict.copy()
-        a_model_not_required_nullable_model = cls()
+
+        def _parse_a_property(data: Any) -> Union[Unset, AnEnum, AnIntEnum]:
+            data = None if isinstance(data, Unset) else data
+            a_property: Union[Unset, AnEnum, AnIntEnum]
+            try:
+                a_property = UNSET
+                _a_property = data
+                if _a_property is not None and _a_property is not UNSET:
+                    a_property = AnEnum(_a_property)
+
+                return a_property
+            except:  # noqa: E722
+                pass
+            a_property = UNSET
+            _a_property = data
+            if _a_property is not None and _a_property is not UNSET:
+                a_property = AnIntEnum(_a_property)
+
+            return a_property
+
+        a_property = _parse_a_property(d.pop("a_property", UNSET))
+
+        a_model_not_required_nullable_model = cls(
+            a_property=a_property,
+        )
 
         a_model_not_required_nullable_model.additional_properties = d
         return a_model_not_required_nullable_model
